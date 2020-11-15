1ecb66304839134f5869ed52c160341346cbdcb8
Isaiah Headley <36669288+IsaiahHeadley@users.noreply.github.com>
2020-11-15 13:43:14 -0500

    Update operations.py

--- operations.py
+++ operations.py
@@ -1,4 +1,4 @@
-# this is a simple python program that calculates operations
+# this is a simple python program that calculates mathmatical operations
 print(1*2) # = 2
 print(2/2) # = 1
 print(1+1) # = 2
