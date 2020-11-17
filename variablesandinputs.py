29966a748ac0da199b1329281bbeb2d65e30e5ed
Isaiah Headley <36669288+IsaiahHeadley@users.noreply.github.com>
2020-11-16 19:26:55 -0500

    Add files via upload

---
+++ variablesandinputs.py
@@ -0,0 +1,23 @@
+# this is a simple python program that exhibits variables and input
+
+user = "Elijah" #assigning a variable named "user" and giving it the value Elijah
+
+print(user)
+
+x = "Bacon" #assigning a variable named x and giving it the value of Bacon 
+
+print(user + " loves "+  x + "!") # String concatenation Elijah loves Bacon!
+
+y = input("please type something you would like to input and hit enter ")#this is simple input
+
+print(y)
+
+age1 = int(input("Please enter your age "))# here we are taking input of an integer and assigning it to the age1 variable
+
+
+print("your age is " + str(age1)) #string concatenation
+
+
+
+age2 = 12
+print("My age is " + str(age2))  #My age is 42.
