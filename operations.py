9df32e1c5ec6e634814620813fa25927075e3954
Isaiah Headley <36669288+IsaiahHeadley@users.noreply.github.com>
2021-01-19 16:33:21 -0500

    Update operations.py

--- operations.py
+++ operations.py
@@ -1,5 +1,14 @@
-# this is a simple python program that calculates mathmatical operations
-print(1*2) # = 2
-print(2/2) # = 1
-print(1+1) # = 2
-print(1-1) # = 0
+print('this is a simple python program that calculates mathmatical operations')
+print('1+2 is..' +str(1+2)) #3
+print('2-1 is..' +str(2-1)) #1
+print('1x2 is..' +str(1*2)) #2
+print('2/2 is..' +str(2/2)) #1
+
+
+
+#old scrpt below 
+
+#print(1+2) # = 2
+#print(2-2) # = 0
+#print(1+1) # = 2
+#print(1-1) # = 0
